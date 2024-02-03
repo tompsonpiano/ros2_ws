e5c2c08780d253f4d89872b2f03c224bcbd31e01
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-02-04 02:47:41 +0900

    main push

--- src/mypkg/mypkg/talker.py
+++ src/mypkg/mypkg/talker.py
@@ -8,9 +8,27 @@ def cb(request, response):
     else:
         response.age = 255
 
+<<<<<<< HEAD
     return response
  
 rclpy.init()
 node = Node("talker")
 srv = node.create_service(Query, "query", cb)
+=======
+class Talker():
+    def __init__(self, node):
+        self.pub = node.create_publisher(Int16, "countup", 10)
+        self.n = 0
+        node.create_timer(0.5, self.cb)
+
+    def cb(self):
+        msg = Int16()
+        msg.data = self.n
+        self.pub.publish(msg)
+        self.n += 1
+
+rclpy.init()
+node = Node("talker")
+talker = Talker(node)
+>>>>>>> lesson10
 rclpy.spin(node)
