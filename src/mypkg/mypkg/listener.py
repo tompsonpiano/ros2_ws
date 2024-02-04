05946a0ee672d50371852cc712bcb4024e80cdad
tompsonpiano <s19c1094hn@s.chibakoudai.jp>
2024-02-04 16:21:49 +0900

    file adjustment

--- src/mypkg/mypkg/listener.py
+++ src/mypkg/mypkg/listener.py
@@ -2,11 +2,14 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
 
-def cb(msg):
-    global node
-    node.get_logger().info("Listen: %d" % msg.data)
+class Listener():
+
+    def cb(msg):
+        #global node
+        node.get_logger().info("Listen: %d" % msg.data)
 
 rclpy.init()
 node = Node("listener")
-pub = node.create_subscription(Int16, "countup", cb, 10)
+#pub = node.create_subscription(Int16, "countup", cb, 10)
+listner = Listener(node)
 rclpy.spin(node)
